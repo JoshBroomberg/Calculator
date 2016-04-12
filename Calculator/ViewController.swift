8300a91b319c1d303d77acbd746abafb53fbcc69
JoshBroomberg <joshbroomberg@gmail.com>
2016-04-12 02:41:57 -0400

    latest changes

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var inputInProcess = false
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if inputInProcess {
+            display.text = display.text! + digit
+        }
+        else {
+            display.text = digit
+            inputInProcess = true
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var operatingStack = Array<Double>()
+    @IBAction func enter() {
+        inputInProcess = false
+        operatingStack.append(displayValue)
+        print("\(operatingStack)")
+        
     }
-
-
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if inputInProcess{
+            enter()
+        }
+        
+        switch operation {
+            case "*": performOperation {$0 * $1}
+            case "+": performOperation {$0 + $1}
+            case "-": performOperation {$1 - $0}
+            case "/": performOperation {$1 / $0}
+            case "√": performSingleOperation {sqrt($0)}
+            default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operatingStack.count >= 2 {
+            displayValue = operation(operatingStack.removeLast(), operatingStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performSingleOperation(operation: Double -> Double) {
+        if operatingStack.count >= 2 {
+            displayValue = operation(operatingStack.removeLast())
+            enter()
+        }
+    }
+    
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set {
+            display.text = "\(newValue)"
+            inputInProcess = false
+        }
+    }
 }
 
